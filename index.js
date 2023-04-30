a39735b82066fd62c25bf35a93345a36d1638364
Nadia Tsygankova <nadiapritchina@gmail.com>
2023-04-30 17:49:03 -0400

    add toggle

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
 const modalText = document.getElementById('modal-text')
+const declineBtn = document.getElementById('decline-btn')
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -10,6 +12,13 @@ setTimeout(function(){
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
+
+
+
+declineBtn.addEventListener('mouseenter', function(){
+  modalChoiceBtns.classList.toggle('reverse')
+})
+
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
